56b356acc7fed421704f2afbe516ea6e8ca54fe7
Shivraj-Kore <shivrajkore100@gmail.com>
2024-01-02 15:57:18 +0530

    changes added

--- src/components/ParticleBG.js
+++ src/components/ParticleBG.js
@@ -16,8 +16,84 @@ const ParticleBG = () => {
                 top: 0,
                 left: 0,
                 zIndex: -1,
+<<<<<<< Updated upstream
                 width: "100%",
                 height: "100%",
+=======
+                width: '100%',
+                height: '100%',
+            }}  
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "#111624",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+>>>>>>> Stashed changes
             }}
         >
             <Particles
