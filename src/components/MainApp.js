446214c084294a9e486f63e6401183b1997e175e
Your Name <shlokjagtap123@gmail.com>
2024-01-02 15:45:04 +0530

    Shlok made changes

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -1,35 +1,38 @@
 import React from "react";
 
-function MainApp(){
-    return(
+function MainApp() {
+    return (
         <>
-            <div className="container">
-                <div id="task-container">
-                    <div id="form-wrapper">
-                        <form id="form">
-                            <div className="flex-wrapper">
-                                <div style={flexstyle}>
-                                    <input className="form-control" id="title" type="text" placeholder="Add Task"/>
-                                </div>
-                                <div style={flexstyle2}>
-                                    <input className="btn btn-warning" type="submit" name="Add" id="submit" />
-                                </div>
+            <div className="min-h-screen flex items-center justify-center ">
+                <div className="bg-white bg-opacity-80 p-8 rounded shadow-lg">
+                    <form className="mb-8">
+                        <div className="flex items-center">
+                            <div className="w-3/4 mr-2">
+                                <input
+                                    className="w-full py-2 px-4 border border-gray-300 rounded"
+                                    id="title"
+                                    type="text"
+                                    placeholder="Add Task"
+                                />
                             </div>
-                        </form>
+                            <div className="w-1/4">
+                                <button
+                                    className="w-full bg-yellow-500 py-2 px-4 rounded text-white"
+                                    type="submit"
+                                >
+                                    Add
+                                </button>
+                            </div>
+                        </div>
+                    </form>
 
-
-                    </div>
                     <div id="list-wrapper">
-
+                        {/* Add any additional styles for the task list container */}
                     </div>
                 </div>
             </div>
         </>
     );
 }
-
-
-let flexstyle = {flex:6}
-let flexstyle2 = {flex:1}
 
 export default MainApp;
